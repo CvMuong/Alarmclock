90491028deb28a34db251be8e5acf8774dab2fa0
CvMuong <vmuongg203@gmail.com>
2023-09-08 00:00:02 +0700

    new

--- passwordgenerator.py
+++ passwordgenerator.py
@@ -7,15 +7,35 @@ special_char = string.punctuation
 
 alphabet = letter + digits + special_char
 
-pwd_length = 12
+your_name = input("Enter your name: ")
 
-while True:
-    pwd = ""
+pwd_length = 5
 
-    for i in range(pwd_length):
-        pwd += ''.join(secrets.choice(alphabet))
+rp = 0
 
-    if(any(char in special_char for char in pwd) and sum(char in digits for char in pwd)>=2):
-        break
+while rp == 0:
 
-print(pwd)
+    while True:
+        pwd = ""
+
+        for i in range(pwd_length):
+            pwd += ''.join(secrets.choice(alphabet))
+
+        if(any(char in special_char for char in pwd) and sum(char in digits for char in pwd)>=2):
+            break
+
+    pwd = your_name + pwd
+
+    print(f"\n==> Your password: {pwd}")
+
+    rp = input("\n0 = Reset \t 1 = OK: \nChoose 0 or 1: ")
+    rp = int(rp)
+
+print(f"The generated password is: {pwd}")
+
+file = open("archivefiles.csv", "w", encoding="utf8")
+message = input("Enter message: ")
+file.write(message + "\n")
+file.write(pwd)
+file.close()
+
