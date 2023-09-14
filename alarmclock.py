4eaafa9007754251182ba7400d07a98cb665b51a
CvMuong <vmuongg203@gmail.com>
2023-09-15 00:34:25 +0700

    new

--- alarmclock.py
+++ alarmclock.py
@@ -2,25 +2,37 @@ from tkinter import *
 import datetime
 import time as tm
 import winsound
+from threading import *
+from playsound import playsound 
 
-def alarm(set_alarm_timer):
+clock = Tk()
+
+clock.geometry("370x200")
+
+def Threading():
+    t1 = Thread(target = alarm)
+    t1.start()
+
+def alarm():
     while True:
+        set_alarm_timer = f"{hour.get()}:{min.get()}:{sec.get()}"
+        tm.sleep(1)
         current_time = datetime.datetime.now()
-        tm.sleep(10)
         now = current_time.strftime("%H:%M:%S")
         date = current_time.strftime("%d/%m/%Y")
         print("The Set Date is:",date)
         print(now)
         if now == set_alarm_timer:
             print("Time to Wake up")
-        winsound.PlaySound("sound.wav",winsound.SND_ASYNC)
-        break
+            # winsound.PlaySound("sound.wav",winsound.SND_ASYNC)
+            playsound("alarmsound.mp3")
+            break
 
-def actual_time():
-    set_alarm_timer = f"{hour.get()}:{min.get()}:{sec.get()}"
-    alarm(set_alarm_timer)
+# def actual_time(hour, min, sec):
+    # set_alarm_timer = f"{hour.get()}:{min.get()}:{sec.get()}"
+#     alarm(set_alarm_timer)
 
-clock = Tk()
+# clock = Tk()
 
 def time():
     current_time = datetime.datetime.now()
@@ -29,7 +41,7 @@ def time():
     label.after(1000, time)
 
 clock.title("DataFlair Alarm Clock")
-clock.geometry("370x200")
+# clock.geometry("370x200")
 clock.configure(background="black")
 time_format=Label(clock, text= "Enter time in 24 hour format!", fg="red",bg="black",font="Arial").place(x=60,y=150)
 # addTime = Label(clock,text = "Hour  Min   Sec",font=60).place(x = 150)
@@ -54,11 +66,12 @@ minTime= Entry(clock,textvariable = min,bg = "pink",width = 10).place(x=160,y=55
 secTime = Entry(clock,textvariable = sec,bg = "pink",width = 10).place(x=270,y=55)
 
 #To take the time input by user:
-submit = Button(clock,text = "Set Alarm",fg="black", bg= "white", width = 10,command = actual_time()).place(x =150,y=90)
+# submit = Button(clock,text = "Set Alarm",fg="black", bg= "white", width = 10,command = actual_time(hour, min, sec)).place(x =150,y=90)
 label = Label(clock, font=("ds-digital", 11), background = "black", foreground = "cyan")
 label.place(x=220, y=123)
 time()
 timenow = Label(clock, text = f"Thís time now is: ", bg = "black", fg = "white", font = 10).place(x=60,y=120)
+Button(clock,text = "Set Alarm",fg="black", bg= "white", width = 10,command = Threading).place(x =150,y=90)
 
 clock.mainloop()
 #Execution of the window.
